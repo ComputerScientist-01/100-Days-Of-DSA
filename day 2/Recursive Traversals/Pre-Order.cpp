c0ff68081e8b405ac540c56ef987fe71f3161623
ComputerScientist-01 <rathi406@gmail.com>
2021-06-24 17:56:13 +0530

    📦 NEW: Preorder Recursive

---
+++ day 2/Recursive Traversals/Pre-Order.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    } 
+};
+
+void PreOrder(struct Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    cout << root->data <<" ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+
+
+            //    1 
+            //  /  \ 
+            //  2   3
+            // / \ / \ 
+            // 4 5 6 7
+
+int main()
+{
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    PreOrder(root);
+    return 0;
+}
