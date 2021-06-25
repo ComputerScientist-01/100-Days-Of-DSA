5d9b41d13034a021e707e895a3ad4a41c67de9e5
ComputerScientist-01 <rathi406@gmail.com>
2021-06-25 23:48:36 +0530

    📦 NEW: Inorder-Postorder-tree

---
+++ day 3/build-tree-from-inorder-and-postorder.cpp
@@ -0,0 +1,74 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    } 
+};
+
+void InOrder(struct Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    InOrder(root->left);
+    cout << root->data <<" ";
+    InOrder(root->right);
+} 
+int search(int inorder[], int start,int end, int curr)
+{
+    for (int i = start; i <= end; i++)
+    {
+        if(inorder[i]==curr)
+        return i;
+    }
+    return -1;
+    
+}
+
+
+Node* buildTree(int postorder[], int inorder[],int start,int end) {
+    static int idx = 4;
+    if(start > end) {
+        return NULL;
+    }
+    int curr =postorder[idx];
+    idx--;
+    Node* node = new Node(curr);
+    
+    if(start == end) {
+        return node;
+    }
+
+
+    int pos = search(inorder,start,end,curr);
+    node->right = buildTree(postorder,inorder,pos+1,end);
+    node->left = buildTree(postorder,inorder,start,pos-1);
+    
+
+    return node;
+}
+
+int main()
+{
+    int inorder[]={4,2,1,5,3};
+    int postorder[]={4,2,5,3,1};
+    
+    
+    Node* root = buildTree(postorder,inorder,0,4);
+
+    InOrder(root);
+    
+    return 0;
+}
+
