6a35f400b9a2d08fc441217e7c46dba0c3541678
ComputerScientist-01 <rathi406@gmail.com>
2021-09-17 14:15:18 +0530

    New : Fibonacci Number

---
+++ Dynamic Programming/Fibonnaci.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        int fib[n + 2];
+        fib[0] = 0;
+        fib[1] = 1;
+        int it = 0;
+        for (it = 2; it <= n; it++)
+        {
+            fib[it] = fib[it - 1] + fib[it - 2];
+        }
+        return fib[n];
+    }
+};
+
+// using dp we reduced time complextity from O(2**n) to O(n)
